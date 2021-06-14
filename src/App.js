7ba0c5479182627b1930bea5bc23fe99514d1d72
Joshua Haddock <joshhaddock88@gmail.com>
2021-06-14 14:58:39 -0700

    Add images. Build basic bones for site.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
 import './App.css';
-// import React from 'react';
+import React from 'react';
+import Main from './Main.js';
+
 
 class App extends React.Component {
   render () {
     return (
-      <h1>Hello</h1>
+      <>
+        <h1>Hello</h1>
+        <Main />
+      </>
     )
   }
 }
