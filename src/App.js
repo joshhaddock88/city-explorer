0f2788af11d8a8a22d7e5d71738c5f5a4959e0f5
Joshua Haddock <joshhaddock88@gmail.com>
2021-06-14 13:47:48 -0700

    Make initial site work, delete tests, create .env etc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+// import React from 'react';
 
+class App extends React.Component {
+  render () {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
 
 
 export default App;
